9e8d2b5ff374612e24686d8dc771f2054e88f4b7
Pier-Luc Brault <pier-luc@brault.me>
2023-02-22 10:15:36 -0500

    Implement echo in TCP (#1)

--- Client/client-main.cpp
+++ Client/client-main.cpp
@@ -5,5 +5,52 @@ using namespace std;
 
 int main()
 {
-	system("pause");
+    sf::TcpSocket socket;
+    sf::SocketSelector selecteur;
+    sf::IpAddress adresseServeur = "127.0.0.1";
+
+    unsigned short portServeur = 54000;
+    unsigned short portClient;
+
+    string messageSortant;
+    string messageEntrant;
+    sf::Packet paquetSortant;
+    sf::Packet paquetEntrant;
+
+    setlocale(LC_ALL, "");
+
+    // Se connecter au serveur
+    if (socket.connect(adresseServeur, portServeur)) {
+        cout << "Une erreur est survenue lors de la connexion au serveur." << endl;
+        return 1;
+    }
+    portClient = socket.getLocalPort();
+
+    // On associe le socket à un sélecteur.
+    // Celui-ci permettra de mettre un temps d'attente maximal sur la réception de données.
+    selecteur.add(socket);
+
+    cout << "Écho Réseau" << endl;
+    cout << "===========" << endl << endl;
+
+    cout << "Connecté au serveur " << adresseServeur << "." << endl;
+    cout << "Le client écoute sur le port " << portClient << "." << endl << endl;
+
+    // Tant que le programme n'est pas arrêté par l'utilisateur
+    while (true) {
+        cout << "Entrer un message: ";
+        getline(cin, messageSortant);
+        paquetSortant << messageSortant;
+
+        socket.send(paquetSortant);
+
+        socket.receive(paquetEntrant);
+
+        paquetEntrant >> messageEntrant;
+
+        cout << "Le serveur (" << socket.getRemoteAddress() << ":" << socket.getRemotePort()
+             << ") a retourné : " << messageEntrant << endl;
+
+        paquetSortant.clear(); // On efface le contenu de paquetSortant pour pouvoir le réutiliser
+    }
 }
--- Server/server-main.cpp
+++ Server/server-main.cpp
@@ -5,5 +5,77 @@ using namespace std;
 
 int main()
 {
-	system("pause");
+    sf::TcpListener listener;
+    sf::TcpSocket* nouveauClient;
+    vector<sf::TcpSocket*> clients; // Vecteur dynamique contenant les sockets de tous les clients
+    sf::SocketSelector selecteur;
+
+    sf::Socket::Status etatClient;
+    sf::Packet paquetEntrant;
+    sf::Packet paquetSortant;
+
+    unsigned short portServeur = 54000;
+    unsigned short portClient;
+
+    string message;
+
+    setlocale(LC_ALL, "");
+
+    // Attacher le listener au port du serveur
+    if (listener.listen(portServeur) != sf::Socket::Done) {
+        cout << "Une erreur est survenue lors de la création du listener." << endl;
+        return 1;
+    }
+
+    // Ajouter le listener au sélecteur
+    selecteur.add(listener);
+
+    cout << "Le serveur écoute sur le port " << portServeur << "." << endl;
+
+    // Tant que le programme n'est pas arrêté par l'utilisateur
+    while (true) {
+        // Attendre un maximum de 10 ms que des données soient reçues sur un socket (incluant le listener)
+        if (selecteur.wait(sf::milliseconds(10.f))) {
+            // Si le listener a reçu une nouvelle demande de connexion
+            if (selecteur.isReady(listener)) {
+                nouveauClient = new sf::TcpSocket();
+                listener.accept(*nouveauClient);
+                clients.push_back(nouveauClient); // On ajoute le socket du nouveau client au vecteur de clients
+                selecteur.add(*nouveauClient); // Il faut aussi ajouter le socket au sélecteur
+
+                cout << "Un nouveau client s'est connecté: "
+                     << nouveauClient->getRemoteAddress() << ":" << nouveauClient->getRemotePort() << endl;
+            }
+
+            // Itérer sur les sockets de tous les clients
+            for (int i = 0; i < clients.size(); i++) {
+                // Si ce socket d'un client a reçu de nouvelles données
+                if (selecteur.isReady(*clients[i])) {
+                    etatClient = clients[i]->receive(paquetEntrant);
+
+                    // Détecter la déconnexion du client.
+                    // À la déconnexion, on reçoit un Packet vide, donc on ne le traitera pas.
+                    if (etatClient == sf::Socket::Disconnected) {
+                        cout << "Le client " << clients[i]->getRemoteAddress()
+                             << ":" << clients[i]->getRemotePort()
+                             << " s'est déconnecté." << endl;
+                        selecteur.remove(*clients[i]); // Retirer le socket client du sélecteur
+                        clients.erase(clients.begin() + i); // Retirer le socket client du vecteur
+                    }
+                    else {
+                        paquetEntrant >> message;
+
+                        cout << clients[i]->getRemoteAddress() << ":" << clients[i]->getRemotePort()
+                             << " a envoyé: " << message << endl;
+
+                        // Renvoyer le même message au client
+                        paquetSortant << message;
+                        clients[i]->send(paquetSortant);
+                    }
+                }
+            }
+        }
+
+        paquetSortant.clear(); // On efface le contenu de paquetSortant pour pouvoir le réutiliser
+    }
 }
