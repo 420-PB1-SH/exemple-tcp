e48e7780547cda659c00312c6e631a36a304518e
Pier-Luc Brault <pier-luc@brault.me>
2024-02-19 10:11:47 -0500

    Enlever la condition pour le wait et déplacer paquetSortant.clear

--- Server/server-main.cpp
+++ Server/server-main.cpp
@@ -35,57 +35,57 @@ int main()
     // Tant que le programme n'est pas arrêté par l'utilisateur
     while (true) {
         // Attendre que des données soient reçues sur un socket (incluant le listener)
-        if (selecteur.wait()) {
-            // Si le listener a reçu une nouvelle demande de connexion
-            if (selecteur.isReady(listener)) {
-                nouveauClient = new sf::TcpSocket();
-                listener.accept(*nouveauClient);
-                clients.push_back(nouveauClient); // On ajoute le socket du nouveau client au vecteur de clients
-                selecteur.add(*nouveauClient); // Il faut aussi ajouter le socket au sélecteur
+        selecteur.wait();
 
-                cout << "Un nouveau client s'est connecté: "
-                     << nouveauClient->getRemoteAddress() << ":" << nouveauClient->getRemotePort() << endl;
-            }
+        // Si le listener a reçu une nouvelle demande de connexion
+        if (selecteur.isReady(listener)) {
+            nouveauClient = new sf::TcpSocket();
+            listener.accept(*nouveauClient);
+            clients.push_back(nouveauClient); // On ajoute le socket du nouveau client au vecteur de clients
+            selecteur.add(*nouveauClient); // Il faut aussi ajouter le socket au sélecteur
 
-            // Itérer sur les sockets de tous les clients
-            for (int i = 0; i < clients.size(); i++) {
-                // Si ce socket d'un client a reçu de nouvelles données
-                if (selecteur.isReady(*clients[i])) {
-                    etatClient = clients[i]->receive(paquetEntrant);
+            cout << "Un nouveau client s'est connecté: "
+                    << nouveauClient->getRemoteAddress() << ":" << nouveauClient->getRemotePort() << endl;
+        }
 
-                    // Détecter la déconnexion du client.
-                    // À la déconnexion, on reçoit un Packet vide, donc on ne le traitera pas.
-                    if (etatClient == sf::Socket::Disconnected) {
-                        cout << "Le client " << clients[i]->getRemoteAddress()
-                            << ":" << clients[i]->getRemotePort()
-                            << " s'est déconnecté." << endl;
+        // Itérer sur les sockets de tous les clients
+        for (int i = 0; i < clients.size(); i++) {
+            // Si ce socket d'un client a reçu de nouvelles données
+            if (selecteur.isReady(*clients[i])) {
+                etatClient = clients[i]->receive(paquetEntrant);
 
-                        selecteur.remove(*clients[i]); // Retirer le socket client du sélecteur
+                // Détecter la déconnexion du client.
+                // À la déconnexion, on reçoit un Packet vide, donc on ne le traitera pas.
+                if (etatClient == sf::Socket::Disconnected) {
+                    cout << "Le client " << clients[i]->getRemoteAddress()
+                        << ":" << clients[i]->getRemotePort()
+                        << " s'est déconnecté." << endl;
+
+                    selecteur.remove(*clients[i]); // Retirer le socket client du sélecteur
                         
-                        delete clients[i];
-                        clients[i] = nullptr;
+                    delete clients[i];
+                    clients[i] = nullptr;
                         
-                        clients.erase(clients.begin() + i); // Retirer le socket client du vecteur
-                    }
-                    else {
-                        paquetEntrant >> message;
+                    clients.erase(clients.begin() + i); // Retirer le socket client du vecteur
+                }
+                else {
+                    paquetEntrant >> message;
 
-                        cout << clients[i]->getRemoteAddress() << ":" << clients[i]->getRemotePort()
-                             << " a envoyé: " << message << endl;
+                    cout << clients[i]->getRemoteAddress() << ":" << clients[i]->getRemotePort()
+                            << " a envoyé: " << message << endl;
 
-                        // Renvoyer le même message au client
-                        paquetSortant << message;
-                        clients[i]->send(paquetSortant);
-                    }
+                    // Renvoyer le même message au client
+                    paquetSortant << message;
+                    clients[i]->send(paquetSortant);
+
+                    paquetSortant.clear(); // On efface le contenu de paquetSortant pour pouvoir le réutiliser
                 }
             }
         }
-
-        paquetSortant.clear(); // On efface le contenu de paquetSortant pour pouvoir le réutiliser
     }
 
     for (int i = 0; i < clients.size(); i++) {
-		delete clients[i];
+        delete clients[i];
         clients[i] = nullptr;
-	}
+    }
 }
